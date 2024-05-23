ee6baefdbd2cf470b22b20d088740607f5d513d9
Tejas Sinkar <tejassinkar10@gmail.com>
2024-05-23 16:29:31 +0530

    Time: 53 ms (40.20%) | Memory: 16.6 MB (98.96%) - LeetSync

--- 496-next-greater-element-i/next-greater-element-i.py
+++ 496-next-greater-element-i/next-greater-element-i.py
@@ -1,15 +1,22 @@
 class Solution:
-	def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
-		result = []
-		for i in range(len(nums1)):
-			start = False
-			for j in range(len(nums2)):
-				if nums1[i]==nums2[j]:
-					start = True
-				if start and nums2[j] > nums1[i]:
-					result.append(nums2[j])
-					start = False
-					break
-			if start==True:
-				result.append(-1)
-		return result
+    def nextGreaterElement(self, nums1: List[int], nums2: List[int]) -> List[int]:
+        # Dictionary to hold the next greater element for each number in nums2
+        next_greater = {}
+        # Stack to keep track of the numbers for which we haven't found the next greater element yet
+        stack = []
+
+        # Traverse nums2 to fill the next_greater dictionary
+        for num in nums2:
+            # While stack is not empty and the current number is greater than the stack's top element
+            while stack and stack[-1] < num:
+                # Pop the stack and set the current number as the next greater element for the popped number
+                next_greater[stack.pop()] = num
+            # Push the current number onto the stack
+            stack.append(num)
+
+        # For any remaining numbers in the stack, there is no greater element, so set it to -1
+        while stack:
+            next_greater[stack.pop()] = -1
+
+        # Construct the result list for nums1 using the next_greater dictionary
+        return [next_greater.get(num, -1) for num in nums1]
