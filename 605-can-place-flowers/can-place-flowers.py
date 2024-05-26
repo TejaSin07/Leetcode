a1fc8110f4c04d74b74bc58af02e469c0fd839ae
Tejas Sinkar <tejassinkar10@gmail.com>
2024-05-26 10:59:16 +0530

    Time: 204 ms (5.07%) | Memory: 17 MB (33.82%) - LeetSync

--- 605-can-place-flowers/can-place-flowers.py
+++ 605-can-place-flowers/can-place-flowers.py
@@ -3,7 +3,7 @@ class Solution:
         if n == 0:
             return True
         for i in range(len(flowerbed)):
-            if flowerbed[i] == 0 and ( i == 0 or flowerbed[i-1]==0) and ( i == len(flowerbed)-1 or flowerbed[i+1]==0  ):
+            if flowerbed[i] == 0 and (i == 0 or flowerbed[i-1] == 0) and (i == len(flowerbed)-1 or flowerbed[i+1] == 0):
                 flowerbed[i] =1
                 n = n-1
                 if n == 0:
@@ -12,5 +12,5 @@ class Solution:
             
 
 
-
+        
         
