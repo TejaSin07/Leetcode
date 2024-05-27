966835fe029377bc79c8a58126e27ab3ca401192
Tejas Sinkar <tejassinkar10@gmail.com>
2024-05-27 16:18:10 +0530

    Time: 49 ms (5.76%) | Memory: 16.7 MB (78.27%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.py
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.py
@@ -1,6 +1,32 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
-        words = s.split()
-        words.reverse()
-        return ' '.join(words)
+        words = []
+        slow, fast = 0, 0
         
+        # Use the first char to determine if we're starting on a " " or a word
+        mode = 'blank' if s[0] == ' ' else 'word'
+        
+        while fast < len(s):
+            # If we start on a word and our fast ptr lands on a white space
+            # means that we have singled out a word
+            if mode == 'word' and s[fast] == ' ':
+                words.append(s[slow:fast])
+                slow = fast # Make the slow ptr catch up
+                mode = 'blank'
+                
+            # If we start on a white space and our fast ptr runs into a character
+            # means we are at the start of a word   
+            elif mode == 'blank' and s[fast] != ' ':
+                slow = fast  # Make the slow ptr catch up
+                mode = 'word'
+        
+            fast += 1  # Increment the fast pointer
+        
+        # Append the last word
+        # Edge cases where the last chunk of string are white spaces
+        if (lastWord := s[slow:fast]).strip():
+            words.append(lastWord)
+        
+        return ' '.join(words[::-1])
+
+
