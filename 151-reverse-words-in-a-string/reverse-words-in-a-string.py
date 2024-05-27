5f4d691f887ab2d0b689a0014c8b39926058cd04
Tejas Sinkar <tejassinkar10@gmail.com>
2024-05-27 16:36:34 +0530

    Time: 43 ms (16.96%) | Memory: 16.7 MB (78.27%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.py
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.py
@@ -1,22 +1,25 @@
 class Solution:
     def reverseWords(self, s: str) -> str:
         words = []
-        slow,fast = 0,0
+        slow, fast = 0, 0
+        
         mode = 'blank' if s[0] == ' ' else 'word'
-
+        
         while fast < len(s):
             if mode == 'word' and s[fast] == ' ':
                 words.append(s[slow:fast])
-                slow = fast 
+                slow = fast  # Synchronize slow with fast
                 mode = 'blank'
-
-            elif mode == 'blank' and s[fast]  != ' ':
-                slow = fast 
+                
+            elif mode == 'blank' and s[fast] != ' ':
+                slow = fast  # Synchronize slow with fast
                 mode = 'word'
+        
             fast += 1
+        
+        # Append the last word
         if (lastWord := s[slow:fast]).strip():
             words.append(lastWord)
         
         return ' '.join(words[::-1])
-
 
