8051257c2e7ba2658bdc7d1f6d836ef629228cc8
Tejas Sinkar <tejassinkar10@gmail.com>
2024-05-28 16:17:16 +0530

    Time: 63 ms (66.48%) | Memory: 17.3 MB (71.70%) - LeetSync

--- 1321-get-equal-substrings-within-budget/get-equal-substrings-within-budget.py
+++ 1321-get-equal-substrings-within-budget/get-equal-substrings-within-budget.py
@@ -1,24 +1,13 @@
 class Solution:
     def equalSubstring(self, s: str, t: str, maxCost: int) -> int:
-        # Initialize current cost, left pointer, and result
         curCost = 0
         l = 0
         res = 0
-        
-        # Loop through the string s
-        for r in range(len(s)):
-            # Calculate the cost of converting character s[r] to t[r] and update current cost
-            curCost += abs(ord(s[r]) - ord(t[r]))
 
-            # If the current cost exceeds the maximum allowed cost
-            while curCost > maxCost:
-                # Update current cost by subtracting the cost of converting character s[l] to t[l]
-                curCost -= abs(ord(s[l]) - ord(t[l]))
-                # Move left pointer to the right
-                l += 1
-            
-            # Update the result by taking the maximum of the current result and the window size
-            res = max(res, r - l + 1)
-        
-        # Return the final result
+        for r in range(len(s)):
+            curCost += abs(ord(s[r])-ord(t[r]))
+            while curCost>maxCost:
+                curCost -= abs(ord(s[l])-ord(t[l]))
+                l+=1
+            res = max(res,r-l+1)
         return res
