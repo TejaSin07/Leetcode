587a57148f214290dc4a0d41c0e8d3c19d17a40a
Tejas Sinkar <tejassinkar10@gmail.com>
2024-05-28 17:41:10 +0530

    Time: 65 ms (13.10%) | Memory: 16.6 MB (97.65%) - LeetSync

--- 443-string-compression/string-compression.py
+++ 443-string-compression/string-compression.py
@@ -1,33 +1,20 @@
 class Solution:
-
     def compress(self, chars: List[str]) -> int:
-        res = []  # Initialize an empty list to store the compressed characters
-        r = 0  # Initialize the index for iterating through the characters
-        counter = 1  # Initialize a counter to count consecutive characters
-        
-        # Iterate through the characters starting from the second one
+        res = []
+        r = 0
+        counter = 1
         for r in range(1, len(chars)):
-            # If the current character is the same as the previous one
             if chars[r] == chars[r - 1]:
-                counter += 1  # Increment the counter for consecutive characters
+                counter += 1
             else:
-                res.append(chars[r - 1])  # Append the current character to the result list
-                # If the count of consecutive characters is greater than 1
+                res.append(chars[r - 1])
                 if counter > 1:
-                    # Convert the count to a string and add each digit separately to the result list
-                    for digit in str(counter):
+                    for digit in str(counter):  # Convert counter to string and add each digit separately
                         res.append(digit)
-                counter = 1  # Reset the counter for the new character group
-        
-        # Append the last character to the result list
+                counter = 1
         res.append(chars[-1])
-        # If the count of consecutive characters for the last group is greater than 1
         if counter > 1:
-            # Convert the count to a string and add each digit separately to the result list
-            for digit in str(counter):
+            for digit in str(counter):  # Convert counter to string and add each digit separately
                 res.append(digit)
-        
-        # Update the original list with the compressed version
         chars[:len(res)] = res
-        # Return the length of the compressed array
         return len(res)
