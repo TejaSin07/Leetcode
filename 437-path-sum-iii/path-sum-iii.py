b1d9ab43682cb8345fd88bb955b988bada07444f
Tejas Sinkar <tejassinkar10@gmail.com>
2024-06-08 14:52:30 +0530

    Time: 369 ms (17.82%) | Memory: 16.7 MB (98.56%) - LeetSync

--- 437-path-sum-iii/path-sum-iii.py
+++ 437-path-sum-iii/path-sum-iii.py
@@ -7,29 +7,61 @@
 class Solution:
     def pathSum(self, root: Optional[TreeNode], targetSum: int) -> int:
         if not root:
-            return 0 
+            return 0
 
-        def count_path(node,target):
+        def count_path_sum(node,target):
             if not node:
                 return 0
-            count = 0 
-            if node.val == target :
-                count +=1
-            count += count_path(node.left,target-node.val) 
-            count += count_path(node.right,target-node.val) 
+            count = 0
+            if node.val == target:
+                count+=1
+
+            count += count_path_sum(node.left,target-node.val) 
+
+            count += count_path_sum(node.right,target-node.val)
 
             return count
-
         def traverse(node):
             if not node:
-                return 0 
-            path_from_node = count_path(node,targetSum)
+                return 0
+            count_node_sum = count_path_sum(node,targetSum)
 
-            path_from_left = traverse(node.left)
+            lef_sum = traverse(node.left)
+            rig_sum = traverse(node.right)
 
-            path_from_right = traverse(node.right)
+            return count_node_sum + lef_sum + rig_sum
 
-            return path_from_node + path_from_left + path_from_right
-        return traverse(root)            
+        return traverse(root) 
+
+
+
+
+
+
+        # if not root:
+        #     return 0 
+
+        # def count_path(node,target):
+        #     if not node:
+        #         return 0
+        #     count = 0 
+        #     if node.val == target :
+        #         count +=1
+        #     count += count_path(node.left,target-node.val) 
+        #     count += count_path(node.right,target-node.val) 
+
+        #     return count
+
+        # def traverse(node):
+        #     if not node:
+        #         return 0 
+        #     path_from_node = count_path(node,targetSum)
+
+        #     path_from_left = traverse(node.left)
+
+        #     path_from_right = traverse(node.right)
+
+        #     return path_from_node + path_from_left + path_from_right
+        # return traverse(root)            
 
         
