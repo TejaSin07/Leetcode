27cfc2974f6d14ce5000bbd1a4d3ee9a884d853d
Tejas Sinkar <tejassinkar10@gmail.com>
2024-06-13 11:45:06 +0530

    Time: 127 ms (97.98%) | Memory: 17 MB (74.42%) - LeetSync

--- 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.py
+++ 1392-find-the-difference-of-two-arrays/find-the-difference-of-two-arrays.py
@@ -1,12 +1,19 @@
 class Solution:
     def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
-        k =[]
-        m = []
-        for n in nums1:
-            if n not in nums2 and n not in k:
-                k.append(n)
-        for n in nums2:
-            if n not in nums1 and n not in m:
-                m.append(n)
-                
-        return k,m
+        a = set(nums1)
+        b = set(nums2)
+
+        return [list(a-b),list(b-a)]
+        
+        
+        
+        
+        # k =[]
+        # m = []
+        # for n in nums1:
+        #     if n not in nums2 and n not in k:
+        #         k.append(n)
+        # for n in nums2:
+        #     if n not in nums1 and n not in m:
+        #         m.append(n)        
+        # return k,m
