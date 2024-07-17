1b548fa8218e8a8de85a2ddd7a6b9511c2260e36
Tejas Sinkar <tejassinkar10@gmail.com>
2024-07-17 11:17:14 +0530

    Time: 3 ms (98.54%) | Memory: 42.1 MB (99.11%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -1,39 +1,45 @@
 class Solution {
     public String reverseWords(String s) {
-        if(s==null) return null;
-
+        if(s == null){
+            return null;
+        }
         char[] a = s.toCharArray();
         int n = a.length;
+
         reverse(a,0,n-1);
         reverseWords(a,n);
-        return cleanSpaces(a,n);
+        return cleanSpace(a,n);
     }
 
-    private void reverse(char[] a,int i ,int j){
-        while(i<j){
-            char t  = a[i];
+
+    private void reverse(char[] a,int i ,int j) {
+    while(i<j){
+            char t = a[i];
             a[i++] = a[j];
             a[j--] = t;
-        }
-    }
+        }}
 
-    void reverseWords(char[] a ,int n){
-        int i = 0,j = 0;
-        while(i<n){
-            while(i<j || i < n && a[i] == ' ')i++;
-            while(j<i || j< n && a[j] != ' ')j++;
-            reverse(a,i,j-1);
+        void reverseWords(char[] a,int n){
+            int i = 0 ,j =0;
+            while(i<n){
+                while(i<j || i< n && a[i] == ' ') i++;
+                while(j<i || j < n && a[j] != ' ')j++;
+                reverse(a,i,j-1);
+            }
         }
-    }
 
-    String cleanSpaces(char[] a, int n){
-        int i = 0,j = 0;
-        while(j<n){
-            while(j<n && a[j] == ' ') j++;
-            while(j<n && a[j] != ' ') a[i++]=a[j++];
-            while(j<n && a[j] == ' ') j++;
-            if(j<n) a[i++] = ' '; 
+
+         String cleanSpace(char[] a, int n){
+            int i = 0, j = 0;
+            while(i < n ){
+                while(i< n && a[i] == ' ')i++;
+                while (i<n && a[i] != ' ')a[j++] = a[i++];
+                while(i<n && a[i] == ' ')i++;
+                if(i<n){
+                    a[j++] = ' ';
+                }
+            }
+            return new String(a).substring(0, j);
         }
-        return new String(a).substring(0,i);
-    }
+    
 }
