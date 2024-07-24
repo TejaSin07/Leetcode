b85b05ce67b31041caee4b7ac1d7fc731a2fffb2
Tejas Sinkar <tejassinkar10@gmail.com>
2024-07-24 14:03:09 +0530

    Time: 2 ms (69.22%) | Memory: 41.1 MB (93.12%) - LeetSync

--- 274-h-index/h-index.java
+++ 274-h-index/h-index.java
@@ -1,23 +1,14 @@
  class Solution {
     public int hIndex(int[] citations) {
-        int n = citations.length;
-        int [] bucket = new int [n+1];
-
-        for(int c: citations){
-            if(c >= n){
-                bucket[n]++;
-            }
-            else{
-                bucket[c]++;
-            }
+    Arrays.sort(citations);
+    int n = citations.length;
+    
+    for (int i = 0; i < n; i++) {
+        if (citations[i] >= n - i) {
+            return n - i;
         }
-        int count = 0;
-        for(int i=n;i>=0;i--){
-            count += bucket[i];
-            if(count >= i){
-                return i;
-            }
-        }
-        return 0;
+    }
+    
+    return 0;
     }
 }
