0dd19029097545704138c7115bf4d16b5dc23690
Tejas Sinkar <tejassinkar10@gmail.com>
2024-07-26 10:58:50 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (72.41%) - LeetSync

--- 58-length-of-last-word/length-of-last-word.java
+++ 58-length-of-last-word/length-of-last-word.java
@@ -1,17 +1,16 @@
 class Solution {
     public int lengthOfLastWord(String s) {
         int length = 0;
-// s.charAt(i)
-        for(int i = s.length()-1;i >= 0;i-- ){
-            if(s.charAt(i) == ' '){
-                if(length>0){
+        for(int i=s.length()-1;i>=0;i--){
+            if(s.charAt(i)==' '){
+                if(length > 0){
                     return length;
                 }
+
             }
             else{
-                length += 1;
+                length++;
             }
-            
         }
         return length;
     }
