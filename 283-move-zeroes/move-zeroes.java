34ec83313ac2ef53adb72d4a9c96008a1b14be52
Tejas Sinkar <tejassinkar10@gmail.com>
2024-09-11 10:42:13 +0530

    Time: 2 ms (83.57%) | Memory: 46.3 MB (10.46%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,20 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int j =0;
+        int count=0;
+        for(int i =0;i<nums.length;i++){
+            if(nums[i]!=0){
+                nums[j] = nums[i];
+                j++;
+            }
+            else{
+                count++;
+            }
+        }  
+        while(count !=0){
+            nums[j] = 0;
+            j++;
+            count--;
+        }
+     }
+}
