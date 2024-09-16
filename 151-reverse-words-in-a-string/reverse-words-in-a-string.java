b3ac556203c51373f3647bbf63eb8c3e6699b30e
Tejas Sinkar <tejassinkar10@gmail.com>
2024-09-16 11:56:37 +0530

    Time: 3 ms (99.01%) | Memory: 42.2 MB (98.32%) - LeetSync

--- 151-reverse-words-in-a-string/reverse-words-in-a-string.java
+++ 151-reverse-words-in-a-string/reverse-words-in-a-string.java
@@ -8,38 +8,46 @@ class Solution {
 
         reverse(a,0,n-1);
         reverseWords(a,n);
-        return cleanSpace(a,n);
+        return cleanSpaces(a,n); 
     }
-
-
-    private void reverse(char[] a,int i ,int j) {
-    while(i<j){
+    void reverseWords(char[] a,int n){
+        int i = 0,j=0;
+        while(i<n){
+            while(i<j || i<n && a[i] == ' '){
+                i++;
+            }
+            while(j<i || j< n && a[j] != ' '){
+                j++;
+            }
+            reverse(a,i,j-1);
+        }
+    }
+    public void reverse(char[] a,int i ,int j ){
+        while(i<j){
             char t = a[i];
             a[i++] = a[j];
-            a[j--] = t;
-        }}
-
-        void reverseWords(char[] a,int n){
-            int i = 0 ,j =0;
-            while(i<n){
-                while(i<j || i< n && a[i] == ' ') i++;
-                while(j<i || j < n && a[j] != ' ')j++;
-                reverse(a,i,j-1);
-            }
+            a[j--] = t;   //we update j here not above
         }
 
+    }
 
-         String cleanSpace(char[] a, int n){
-            int i = 0, j = 0;
-            while(i < n ){
-                while(i< n && a[i] == ' ')i++;
-                while (i<n && a[i] != ' ')a[j++] = a[i++];
-                while(i<n && a[i] == ' ')i++;
-                if(i<n){
-                    a[j++] = ' ';
-                }
+    String cleanSpaces(char[] a,int n ){
+        int i = 0,j=0;
+        
+        while (j < n) {
+            while (j < n && a[j] == ' ') {
+                j++;             // skip spaces
             }
-            return new String(a).substring(0, j);
+            while (j < n && a[j] != ' ') {
+                a[i++] = a[j++]; // keep non spaces
+            }
+            while (j < n && a[j] == ' '){
+                j++;             // skip spaces
+            }
+            if (j < n) a[i++] = ' ';                      // keep only one space
         }
+
+        return new String(a).substring(0, i);
+    }
     
 }
