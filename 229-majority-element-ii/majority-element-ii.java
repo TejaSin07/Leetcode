1ede181248d72fbc1e7427dbd0c11ccbd31f17c2
Tejas Sinkar <tejassinkar10@gmail.com>
2024-12-16 18:59:53 +0530

    Time: 4 ms (55.86%) | Memory: 48.1 MB (37.73%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,23 +1,47 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-    List<Integer> aba = new ArrayList<>();
-    HashMap<Integer,Integer> ans = new HashMap<>();
-    int a = nums.length/3;
-        for(int i = 0; i<nums.length;i++){
-          if(ans.containsKey(nums[i])){
-          ans.put(nums[i], ans.get(nums[i]) + 1);
-
-      }
-      else{
-       ans.put(nums[i], 1);
-
-      }
-        }
-         for (Integer key : ans.keySet()){
-            if(ans.get(key) > a){
-              aba.add(key);
+        int count1 = 0,count2 = 0;
+        int cand1= 0,cand2 =0;
+        for(int i = 0;i<nums.length;i++){
+            if(count1 == 0 &&  nums[i] != cand2){
+                count1 =1;
+                cand1= nums[i];
             }
-         }
-         return aba;
+            else  if(count2 == 0 &&  nums[i] != cand1){
+                count2 =1;
+                cand2= nums[i];
+            }
+            else if (nums[i]== cand1){
+                count1++;
+            }
+            else if (nums[i]== cand2){
+                count2++;
+            }
+            else{
+                count1--;
+                count2--;
+            }
+        }
+
+        count1 = 0;
+        count2 = 0;
+        List<Integer> result = new ArrayList<>();
+        int threshold = nums.length/3;
+        for(int i = 0;i<nums.length;i++){
+            if(nums[i] == cand1){
+                count1++;
+            }
+            else if(nums[i]==cand2){
+                count2++;
+            }
+        }
+
+        if(count1 > threshold){
+            result.add(cand1);
+        }
+        if(count2>threshold){
+            result.add(cand2);
+        }
+        return result;
     }
 }
