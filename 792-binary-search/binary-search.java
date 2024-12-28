0e11f9293c67f4e70714d4127802c4e6992d6272
Tejas Sinkar <tejassinkar10@gmail.com>
2024-12-28 20:08:10 +0530

    Time: 3 ms (1.92%) | Memory: 46.4 MB (8.90%) - LeetSync

--- 792-binary-search/binary-search.java
+++ 792-binary-search/binary-search.java
@@ -4,15 +4,14 @@ class Solution {
         int right = nums.length-1;
         while(left <= right){
             int mid = left + (right - left)/2;
-
-            if(nums[mid]== target ){
-                return mid;
-            }
-            else if(nums[mid] > target){
+         if(nums[mid] > target){
                 right = mid-1;
             }
-            else{
+            else if(nums[mid] < target) {
                 left = left +1;
+            }
+            else  {
+                return mid;
             }
         }
         return -1;
