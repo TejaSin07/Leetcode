8b169f32fae033912afd3ade975230edbc71c31c
Tejas Sinkar <tejassinkar10@gmail.com>
2025-01-10 18:08:31 +0530

    Time: 171 ms (33.69%) | Memory: 44.6 MB (34.61%) - LeetSync

---
+++ 1890-sum-of-beauty-of-all-substrings/sum-of-beauty-of-all-substrings.java
@@ -0,0 +1,33 @@
+class Solution {
+    public int beautySum(String s) {
+        int sum=0;
+        for(int start = 0;start < s.length()-1;start++){
+            sum += helperCount( s,start);//aabca
+        }
+        return sum;
+    }
+
+    private static int helperCount(String s,int start){
+        int charAr[] = new int[128];
+        int end = start;
+        int max = 0;
+        
+        int count=0;
+        while(end< s.length()){
+
+             charAr[s.charAt(end)]++;
+             max = Math.max(charAr[s.charAt(end)],max);
+            int min = Integer.MAX_VALUE;
+             for (int freq : charAr) {
+               
+                if (freq > 0) { // Only consider characters that exist in the substring
+                    min = Math.min(min, freq);
+                }
+                
+            }
+             count += (max-min);
+             end++;
+        }
+        return count;
+    }
+}
