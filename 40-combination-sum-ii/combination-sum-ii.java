f812ca78fa8c780520b1b1bfee5c3c8d96383183
Tejas Sinkar <tejassinkar10@gmail.com>
2025-01-25 13:11:09 +0530

    Time: 3 ms (77.93%) | Memory: 43.3 MB (70.63%) - LeetSync

--- 40-combination-sum-ii/combination-sum-ii.java
+++ 40-combination-sum-ii/combination-sum-ii.java
@@ -1,31 +1,31 @@
 class Solution {
     public List<List<Integer>> combinationSum2(int[] candidates, int target) {
         Arrays.sort(candidates);
-        HashSet<List<Integer>> hash = new HashSet<>();
+        List<List<Integer>> ArrayList = new ArrayList<>();
         List<Integer> temp = new ArrayList<>();
-        backTrack(0,candidates,target,temp,hash);
-        List<List<Integer>> arrayList = new ArrayList<>(hash);
+        backTrack(0,candidates,target,temp,ArrayList);
+       
 
-        return arrayList;
+        return ArrayList;
     }
-   private static void backTrack(int index,int[] cand,int target,List<Integer>temp, HashSet<List<Integer>> hash){
+   private static void backTrack(int index,int[] cand,int target,List<Integer>temp, List<List<Integer>> ArrayList){
     if(index == cand.length){
         if(target==0){
             // Collections.sort(temp);
-            hash.add(new ArrayList<>(temp));
+            ArrayList.add(new ArrayList<>(temp));
         }
         return;
     }
 
  if(cand[index]<= target){
             temp.add(cand[index]);
-            backTrack(index+1,cand,target-cand[index],temp,hash);
+            backTrack(index+1,cand,target-cand[index],temp,ArrayList);
             temp.remove(temp.size()-1);
              while(index < cand.length-1  && cand[index] == cand[index+1]   ){
                 index++;
             }
         }
 
-    backTrack(index+1,cand,target,temp,hash);
+    backTrack(index+1,cand,target,temp,ArrayList);
    }
 }
