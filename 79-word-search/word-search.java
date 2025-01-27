55bca862e259bda0983123944437d86455604812
Tejas Sinkar <tejassinkar10@gmail.com>
2025-01-28 00:18:11 +0530

    Time: 149 ms (48.87%) | Memory: 41.8 MB (48.68%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,40 +1,41 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        int m = board[0].length;//colmns
-        int n = board.length; // rows
+        int m = board.length;//ROW
+        int n = board[0].length;//COL
+         int ind = 0;
 
-        for(int i = 0;i< n;i++){
-            for(int j = 0;j< m;j++){
-                if(word.charAt(0)==board[i][j]){
-                    if(helper(i,j,board,word,0,n,m)){
-                        return true;
+         for(int i = 0;i<m;i++){
+               for(int j = 0 ; j<n; j++){
+                   if(board[i][j] == word.charAt(ind)){
+                     if(helper(i , j ,m , n ,  ind , board , word )){
+                    return true;         
                     }
-                }
+                 }
             }
-        }
-        return false;
+       }
+       return false;  
     }
-
-    public boolean helper(int row,int col,char[][] board, String word,int ind,int n,int m){
-
-        if(ind == word.length()) return true;
-
-        if(row<0 || col< 0 || row == n || col == m ||  word.charAt(ind) != board[row][col] ||  board[row][col] == '!' ){
-            return false;
+     public boolean helper(int row , int col ,int m , int n , int ind , char [] [] board , String word){
+        if(ind == word.length()){
+            return true;
         }
-        char c = board[row][col];
+        if(row == m || col == n ||  row<0 || col<0  || board[row][col] == '!' ||  board[row][col] != word.charAt(ind)){
+           return false;
+        }
+        char ch =board[row][col];
         board[row][col] = '!';
+
+        boolean top = helper(row-1 , col ,m , n ,  ind+1 , board , word );
+
+        boolean bottom = helper(row+1 , col ,m , n ,  ind+1 , board , word );
         
-        boolean top    = helper(row-1,col,board,word,ind+1,n,m);
-        boolean bottom = helper(row+1,col,board,word,ind+1,n,m);
-        boolean right  = helper(row,col+1,board,word,ind+1,n,m);
-        boolean left  = helper(row,col-1,board,word,ind+1,n,m);
+        boolean left = helper(row , col-1 ,m , n ,  ind+1 , board , word );
 
-        board[row][col] = c;
+        boolean right = helper(row , col+1 ,m , n ,  ind+1 , board , word );
 
-    return top || bottom || right || left;
-
-    }
-
+        board[row][col] = ch;
+         
+        return top || bottom || left || right;
+     }
     
 }
