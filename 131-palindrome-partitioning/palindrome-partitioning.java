a4751245f1013482904e8ec3ec9bb59c121285eb
Tejas Sinkar <tejassinkar10@gmail.com>
2025-01-27 17:28:33 +0530

    Time: 8 ms (57.71%) | Memory: 56.9 MB (66.14%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -2,35 +2,31 @@ class Solution {
     public List<List<String>> partition(String s) {
         List<List<String>> res = new ArrayList<>();
         List<String> temp = new ArrayList<>();
-
-        backTrack(0,s,temp,res);
+        backTrack(0,s,res,temp);
         return res;
     }
 
-
-    public void backTrack(int ind , String s,List<String> temp,List<List<String>> res){
-        if(ind == s.length() ){
+    public void backTrack(int ind,String s,List<List<String>> res,List<String> temp ){
+        if(ind == s.length()){
             res.add(new ArrayList<>(temp));
             return ;
         }
 
-        for(int i = ind;i < s.length();i++){
-            if(isPali(ind,i,s)){
-                temp.add(s.substring(ind,i+1));
-                backTrack(i+1,s,temp,res);
-                temp.remove(temp.size()-1);
-            }
+    for(int i = ind;i<s.length();i++){
+        if(isPali(ind,i,s)){
+            temp.add(s.substring(ind, i+1));
+            backTrack(i+1,s,res,temp);
+            temp.remove(temp.size()-1);
         }
-
-
+    }
     }
 
-    public boolean isPali(int start ,int end,String s){
-        while(start < end){
+    public boolean isPali(int start,int end,String s){
+        while(end>start){
         if(s.charAt(start++) != s.charAt(end--)){
             return false;
         }
-        }
-        return true;
+    }
+    return true;
     }
 }
