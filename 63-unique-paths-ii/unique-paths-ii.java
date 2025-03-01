02500e1494e2c6014f3441613ced51e722a35464
Tejas Sinkar <tejassinkar10@gmail.com>
2025-03-02 00:47:45 +0530

    Time: 1 ms (20.68%) | Memory: 41.5 MB (90.47%) - LeetSync

--- 63-unique-paths-ii/unique-paths-ii.java
+++ 63-unique-paths-ii/unique-paths-ii.java
@@ -1,35 +1,29 @@
 class Solution {
     public int uniquePathsWithObstacles(int[][] obstacleGrid) {
-    int m = obstacleGrid.length;
-    int n = obstacleGrid[0].length;
-    int dp [][] = new int[m][n];
+        int m =  obstacleGrid.length;
+        int n =   obstacleGrid[0].length;
+        int dp[][] =new int[m][n];
+        
+        for(int row = 0;row < m;row++){
+            for(int col = 0;col<n;col++){
+                if(row == 0 && col ==0) {
+                    if(obstacleGrid[0][0] == 1) return 0;
+                    dp[row][col] = 1;
+                    continue;
+                }
+                int left = 0;
+                int up = 0;
+                if(row >0) up = dp[row-1][col];
+                if(col>0) left = dp[row][col-1];
+                if(obstacleGrid[row][col] != 1){
+                    dp[row][col] = left + up;
+                }
+                else{
+                    dp[row][col] = 0;
+                }
 
-    // Correct way to fill a 2D array with -1
-    for (int i = 0; i < m; i++) {
-        Arrays.fill(dp[i], -1);
-    }
-     return helper(m-1,n-1,dp,obstacleGrid);       
-    }
-
-    public int helper(int row,int col,int[][]dp,int[][]arr){
-        if(arr[row][col] == 1)return 0;
-        if(dp[row][col]  != -1) return dp[row][col];
-        if(row == 0 && col == 0){
-            dp[row][col] = 1;
-            return 1;
+            }
         }
-        int left = 0;
-        int up = 0;
-        if(col >0){
-            left = helper(row,col-1,dp,arr);
-        }
-        if(row>0){
-            up = helper(row-1,col,dp,arr); 
-        }
-
-        dp[row][col] = left + up;
-
-        return dp[row][col];
-
+        return dp[m-1][n-1];
     }
 }
