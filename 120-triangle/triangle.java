38d6c4e82c44997310e094b25cdc1324da950709
Tejas Sinkar <tejassinkar10@gmail.com>
2025-03-04 23:27:52 +0530

    Time: 3 ms (65.21%) | Memory: 43.6 MB (93.95%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -2,29 +2,24 @@ class Solution {
     public int minimumTotal(List<List<Integer>> triangle) {
         int m = triangle.size();
         int n = triangle.get(m-1).size();
-        int[][] dp = new int[m][n];
-        for (int i = 0; i < m; i++) {
-            Arrays.fill(dp[i],-1);
+        int dp[] = new int [n];
+        for(int i = 0 ;i< n;i++){
+            dp[i] = triangle.get(m-1).get(i);
         }
 
-        return helper(0,0,triangle,dp,m);
-    }
-
-    public int helper(int row,int col,List<List<Integer>>tri,int [][]dp,int size){
-        if(row == size-1 ){
-            return tri.get(row).get(col);
+        for(int row = n-2;row>=0;row--){
+            for(int col = 0;col<=row;col++){
+                int down = dp[col];
+                int diag = dp[col+1];
+                dp[col] = Math.min(down,diag) + triangle.get(row).get(col);
+            }
         }
+        return dp[0];
 
-        if(dp[row][col] != -1) return dp[row][col];
 
-        //down
-
-        int down = tri.get(row).get(col) + helper(row+1,col,tri,dp,size);
-        int downRight = tri.get(row).get(col) + helper(row+1,col+1,tri,dp,size);
-
-        return dp[row][col] = Math.min(down,downRight);
-         
+        // 2
+        // 3 4
+        // 6 5 7
+        // 4 1 8 3
     }
-
-    
 }
