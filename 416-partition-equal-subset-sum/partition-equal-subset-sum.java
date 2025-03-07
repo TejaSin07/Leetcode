64d192e37581b9ee4f7819c6efe623359a8c8b87
Tejas Sinkar <tejassinkar10@gmail.com>
2025-03-07 23:33:35 +0530

    Time: 56 ms (38.11%) | Memory: 47 MB (47.21%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -1,44 +1,40 @@
 class Solution {
     public boolean canPartition(int[] nums) {
-        int  n = nums.length;
+        int len = nums.length;
         int sum = 0;
-        for(int a : nums){
-            sum += a;
+        for(int i = 0;i< nums.length;i++){
+            sum += nums[i]; 
         }
-        if(sum%2 == 1){
-            return false;
-        }
+
+        if(sum%2==1)return false;
+
         int target = sum/2;
-        int [][] dp = new int[n][target+1];
-        for(int [] a:dp){
-            Arrays.fill(a,-1);
+
+       boolean dp[][] = new boolean[len][target+1];
+
+        for(boolean temp[] : dp){
+            Arrays.fill(temp,false);
         }
-        return helper(n-1,target,nums,dp); 
+
+        for(int i = 0;i< len;i++){
+            dp[i][0] = true;
+        }
+
+        if(nums[0] <= target){
+            dp[0][nums[0]] = true;
+        }
+
+        for(int i = 1;i< len;i++){
+            for(int t = 1;t<= target;t++){
+                //nontaken
+                boolean  nontake = dp[i-1][t];
+                boolean take = false;
+                if(nums[i] <= t){
+                    take = dp[i-1][t- nums[i]];
+                }
+                dp[i][t] = take || nontake; 
+            }
+        }
+        return dp[len-1][target];
     }
-
-        public   boolean helper(int i,int target,int [] nums,int[][]dp){
-            if(target==0){
-                return true;
-            }
-            if(i == 0){
-                return target == nums[0];
-            }
-
-            if(dp[i][target] != -1){
-                return dp[i][target] == 1;
-            }
-
-            //not taken
-            boolean notTaken = helper(i-1,target,nums,dp);
-            boolean taken = false;
-            
-            //taken
-            if(nums[i] <= target){
-                taken = helper(i-1,target- nums[i],nums,dp);
-            }
-
-            dp[i][target] = (taken || notTaken) ? 1:0;
-
-            return dp[i][target]==1;
-        }
 }
