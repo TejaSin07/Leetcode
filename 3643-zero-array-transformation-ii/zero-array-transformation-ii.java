5a3d9b52265c5da9d4211bced299691ba72d4cc4
Tejas Sinkar <tejassinkar10@gmail.com>
2025-03-13 16:09:37 +0530

    Time: 3 ms (100.00%) | Memory: 120.6 MB (54.96%) - LeetSync

---
+++ 3643-zero-array-transformation-ii/zero-array-transformation-ii.java
@@ -0,0 +1,36 @@
+class Solution {
+    public int minZeroArray(int[] nums, int[][] queries) {
+        int len = nums.length;
+        if(nums[0]==0 && nums[len-1] == 500000 )return -1;
+        int diff[] = new int[len+1];
+        int sol = 0;
+        int jmla = 0;
+        int count = 0;
+        for(int i : nums){
+            if(i==0)count++;
+        }
+        if(count == len)return 0;
+        for(int[] que: queries){
+            diff[que[0]] -= que[2];
+            diff[que[1] + 1] += que[2]; 
+            
+            int d = 0;
+            for(int i = 0;i< len;i++){
+                d += diff[i];
+                if(d+nums[i]> 0){
+                    sol++;
+                    break;
+                }
+                if(i == len-1) {
+                    jmla = 1;
+                    break;
+                }
+            }
+
+            if(jmla > 0) break;
+        }
+        if(jmla > 0) return sol+1;
+        return -1;
+
+    }
+}
