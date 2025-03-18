745bdcb6bfa7b1f0a65163b37dd8d192d47d4485
Tejas Sinkar <tejassinkar10@gmail.com>
2025-03-18 10:32:39 +0530

    Time: 1 ms (100.00%) | Memory: 44.4 MB (72.09%) - LeetSync

--- 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.java
+++ 2308-divide-array-into-equal-pairs/divide-array-into-equal-pairs.java
@@ -1,13 +1,59 @@
 class Solution {
     public boolean divideArray(int[] nums) {
-        Arrays.sort(nums);
-
-        for(int i = 0;i< nums.length;i +=2){
-            if(nums[i] != nums[i+1]){
-                return false;
-            }
+        int len = nums.length;
+        int xor1=0, xor2=0;
+        
+        for(int i=0;i<len;i++){
+            xor1 = nums[i]^xor1;
+            xor2 = (nums[i]+1)^xor2;
         }
-        return true;
 
+        return xor1==0 && xor2==0;
     }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+// class Solution {
+//     public boolean divideArray(int[] nums) {
+//         int len = nums.length;
+//         if(len%2==1)return false;
+//         int first = 0;
+//         int count = 0;
+//         for(int temp : nums){
+//             first = temp^first;
+//             count++;
+//         }
+
+    
+//         if(first == 0) return true;
+//         return false;
+//     }
+// }   
+        
+        
+        
+        
+        
+        
+        
+        
+        // Arrays.sort(nums);
+
+        // for(int i = 0;i< nums.length;i +=2){
+        //     if((nums[i]^nums[i+1]) != 0){
+        //         return false;
+        //     }
+        // }
+        // return true;
+
+    
