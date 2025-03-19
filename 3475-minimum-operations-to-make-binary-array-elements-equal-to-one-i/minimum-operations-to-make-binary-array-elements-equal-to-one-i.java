6c188aaec76d41828c856276c1e3f5e308d3ec1d
Tejas Sinkar <tejassinkar10@gmail.com>
2025-03-19 10:43:15 +0530

    Time: 9 ms (16.02%) | Memory: 57.3 MB (74.95%) - LeetSync

--- 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.java
+++ 3475-minimum-operations-to-make-binary-array-elements-equal-to-one-i/minimum-operations-to-make-binary-array-elements-equal-to-one-i.java
@@ -1,12 +1,15 @@
 class Solution {
+    //IMP NOTE : Svatache Svata karave
     public int minOperations(int[] nums) {
         int result = -1;
-        int sum1=0;
+        //if no need to flip ,checek all are 1
+        int sum1=0;     
         for(int temp: nums){
             sum1 += temp;
         }
         if(sum1 == nums.length) return 0;
 
+        // checking block
         for(int i = 0;i< nums.length-2;i++){
 
             if(nums[i]==1)continue;
@@ -19,13 +22,15 @@ class Solution {
                 if(count==3)break;
             }
         }
+        
+        //check if all are 1 by comparing with length
         int sum = 0;
         for(int temp: nums){
             sum += temp;
         }
 
+        //if not possible then return -1
         if(sum != nums.length) return -1;
-        if(result == -1)return result;
         return result+1;
 
       
