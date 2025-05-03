40145c963d8285e8caecbf34b0a658c38a47d10d
Tejas Sinkar <tejassinkar10@gmail.com>
2025-05-03 22:54:38 +0530

    Time: 0 ms (100.00%) | Memory: 41.2 MB (67.25%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -1,15 +1,17 @@
 class Solution {
     public boolean check(int[] nums) {
-        // here we compare all the neighbouring elemnts and check whether they are in somewhat sorted
-        // there will be a small change due to rotation in the array at only one place.
-        // so if there are irregularities more than once, return false
-        // else return true;
-        int irregularities = 0;
-        int length = nums.length;
-        for (int i=0; i<length; i++) {
-            if (nums[i] > nums[(i + 1) % length])
-                irregularities += 1;
+        int len = nums.length;
+        int rotationCount = 0 ;
+
+        for(int i = 0;i<len;i++){
+            if(nums[i] <= nums[(i+1)%len]){
+                continue;
+            }
+            else{
+                rotationCount++;
+            }
         }
-        return irregularities > 1 ? false : true;
+        if(rotationCount > 1)return false;
+        return true;
     }
 }
