e3a2a24aaab12b0db482985adfcb5257879086f2
Tejas Sinkar <tejassinkar10@gmail.com>
2025-05-03 19:52:42 +0530

    Time: 4 ms (100.00%) | Memory: 44.3 MB (26.68%) - LeetSync

--- 9-palindrome-number/palindrome-number.java
+++ 9-palindrome-number/palindrome-number.java
@@ -1,12 +1,14 @@
 class Solution {
     public boolean isPalindrome(int x) {
-        if(x < 0)return false;
-        int dup = x;
+        if(x < 0 || (x != 0 && x%10 == 0)) return false;
+        // If a number ends with a 0, then its reverse would start with 0, which is not allowed in normal number format (no leading zeros).
+
         int rev = 0;
-        while(dup > 0){
-            rev = rev*10 + dup%10;
-            dup = dup / 10;   
+        while(x>rev){
+            rev = rev *10 + x%10;
+            x = x/10;
         }
-        return rev == x;
+        if(x==rev || x == rev/10)return true;
+        return false;
     }
 }
