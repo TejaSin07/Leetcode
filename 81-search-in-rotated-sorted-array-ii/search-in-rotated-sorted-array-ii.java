147ad6e7aa34990a3000bafd6c0456c1fc76adc6
Tejas Sinkar <tejassinkar10@gmail.com>
2025-05-11 19:14:54 +0530

    Time: 0 ms (100.00%) | Memory: 43.5 MB (27.06%) - LeetSync

--- 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
+++ 81-search-in-rotated-sorted-array-ii/search-in-rotated-sorted-array-ii.java
@@ -1,42 +1,38 @@
 class Solution {
     public boolean search(int[] nums, int target) {
-        int left = 0;
-        int right = nums.length-1;
-        while(left <= right){
-            int mid = (left + right)/2;
+        int left = 0, right = nums.length - 1;
 
-            if(nums[mid]== target){
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            // Target found
+            if (nums[mid] == target) {
                 return true;
             }
-            if(nums[mid]== nums[left]){
+
+            // If duplicates block us from knowing the sorted half, move both pointers
+            if (nums[left] == nums[mid] && nums[mid] == nums[right]) {
                 left++;
-
+                right--;
             }
-            else if(nums[mid] == nums[right]){
-                right --;
+            // Right half is sorted
+            else if (nums[mid] <= nums[right]) {
+                if (target > nums[mid] && target <= nums[right]) {
+                    left = mid + 1; // Target is in the sorted right half
+                } else {
+                    right = mid - 1; // Target is in the left half
+                }
             }
-            else if(nums[mid]<nums[right]){
-                if(target>nums[mid] && target<= nums[right]){
-                    left = mid+1;
+            // Left half is sorted
+            else {
+                if (target >= nums[left] && target < nums[mid]) {
+                    right = mid - 1; // Target is in the sorted left half
+                } else {
+                    left = mid + 1; // Target is in the right half
                 }
-                else{
-                    right = mid-1;
-                }
-            } 
-            else{
-                if(target<nums[mid] && target>= nums[left]){
-                    right = mid-1;
-                }
-                else if(nums[left] == nums[mid]){
-                    left = left+1;
-                }
-
-                else{
-                    left = mid+1;
-                }
-
             }
         }
+
         return false;
     }
 }
