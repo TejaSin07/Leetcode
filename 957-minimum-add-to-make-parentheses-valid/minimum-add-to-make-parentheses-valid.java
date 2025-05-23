eedb2b5508a4f98796c1e026ef6e9e27542f5dfc
Tejas Sinkar <tejassinkar10@gmail.com>
2025-05-23 21:19:59 +0530

    Time: 1 ms (50.86%) | Memory: 41.5 MB (81.67%) - LeetSync

---
+++ 957-minimum-add-to-make-parentheses-valid/minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        Stack<Character> stk = new Stack<>();
+        int count = 0;
+        for(int i = 0;i< s.length();i++){
+            if(s.charAt(i) == '('){
+                stk.push('c');
+            }
+            else if(!stk.empty()){
+                stk.pop();
+            }
+            else{
+                count++;
+            }
+        }
+        return count+stk.size();
+    }
+}
