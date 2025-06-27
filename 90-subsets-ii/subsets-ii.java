fd0d95706a88404837fa45ba003b75069a388832
Tejas Sinkar <tejassinkar10@gmail.com>
2025-06-28 00:21:34 +0530

    Time: 1 ms (99.88%) | Memory: 43.8 MB (19.12%) - LeetSync

--- 90-subsets-ii/subsets-ii.java
+++ 90-subsets-ii/subsets-ii.java
@@ -1,31 +1,26 @@
 class Solution {
     public List<List<Integer>> subsetsWithDup(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        Arrays.sort(nums);
+        List<List<Integer>> lst = new ArrayList<>();
         List<Integer> temp = new ArrayList<>();
-        backTrack(0,nums,result,temp);
-        
-        return result;
+        int len = nums.length;
+        Arrays.sort(nums);
+        helper(0,len,nums,temp,lst);
+        return lst;
     }
 
-
-    private void backTrack(int index,int[] nums,List<List<Integer>> hash,List<Integer> temp ){
-        if(index == nums.length){
-            hash.add(new ArrayList<>(temp));
-            return ;
+    public static void helper(int ind,int len ,int[] nums,List<Integer>temp,List<List<Integer>> lst){
+        if(ind == len){
+            lst.add(new ArrayList<>(temp));
+            return;
         }
 
-        temp.add(nums[index]);
-        
-        backTrack(index+1,nums,hash,temp);
-
+        temp.add(nums[ind]);
+        helper(ind+1,len,nums,temp,lst);
         temp.remove(temp.size()-1);
 
-        while(index < nums.length-1 && nums[index]==nums[index+1]){
-            index++;
+        while(ind < len-1 && nums[ind]==nums[ind+1]){
+            ind++;
         }
-
-        
-        backTrack(index+1,nums,hash,temp);
+        helper(ind+1,len,nums,temp,lst);
     }
 }
