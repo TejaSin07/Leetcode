40c6e24d15b7c86394d3ed7683211b2c687d6028
Tejas Sinkar <tejassinkar10@gmail.com>
2025-06-29 19:19:41 +0530

    Time: 9 ms (26.19%) | Memory: 56.7 MB (79.54%) - LeetSync

--- 131-palindrome-partitioning/palindrome-partitioning.java
+++ 131-palindrome-partitioning/palindrome-partitioning.java
@@ -1,32 +1,32 @@
 class Solution {
     public List<List<String>> partition(String s) {
-        List<List<String>> res = new ArrayList<>();
+        List<List<String>> ans = new ArrayList<>();
         List<String> temp = new ArrayList<>();
-        backTrack(0,s,res,temp);
-        return res;
+        helper(0,temp,ans,s);
+        return ans;
     }
 
-    public void backTrack(int ind,String s,List<List<String>> res,List<String> temp ){
+    public static void helper(int ind,List<String>temp,List<List<String>> ans,String s){
         if(ind == s.length()){
-            res.add(new ArrayList<>(temp));
+            ans.add(new ArrayList<>(temp));
             return ;
         }
 
-    for(int i = ind;i<s.length();i++){
-        if(isPali(ind,i,s)){
-            temp.add(s.substring(ind, i+1));
-            backTrack(i+1,s,res,temp);
-            temp.remove(temp.size()-1);
+        for(int i = ind;i< s.length();i++){
+            if(isPali(ind,i,s)){
+                temp.add(s.substring(ind,i+1));
+                helper(i+1,temp,ans,s);
+                temp.remove(temp.size()-1);
+            }
         }
-    }
     }
 
-    public boolean isPali(int start,int end,String s){
-        while(end>start){
-        if(s.charAt(start++) != s.charAt(end--)){
-            return false;
+    public static boolean isPali(int start,int end,String s){
+        while(start <= end){
+            if(s.charAt(start)!= s.charAt(end))return false;
+            start++;
+            end--;
         }
-    }
-    return true;
+        return true;
     }
 }
