58dcd44f352b892f084470ad2b66c070650b148d
Tejas Sinkar <tejassinkar10@gmail.com>
2025-08-05 22:22:50 +0530

    Time: 0 ms (100.00%) | Memory: 44.3 MB (77.74%) - LeetSync

--- 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
+++ 124-binary-tree-maximum-path-sum/binary-tree-maximum-path-sum.java
@@ -1,35 +1,32 @@
-
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
 class Solution {
-    int max = 0;
-    int count = 0;
+    static int max = Integer.MIN_VALUE;
     public int maxPathSum(TreeNode root) {
-        if(root == null ) return 0;
-        count++;
-        if(count == 1)max = root.val;
-        int left = maxPathSum(root.left) ;//-6
-        int right = maxPathSum(root.right) ;//0
-        count--;
-        
-        if(right < 0 && left < 0){
-            max = Math.max(max,root.val);//-1,
-        }
-        else if(right< 0){
-            max = Math.max(max,left+ root.val);
-        }
-        else if(left<0){
-            max= Math.max(max,right+root.val) ;
-        }
-        else{
-             max = Math.max(max,right+left+root.val);
-        }
+        max = Integer.MIN_VALUE;
+        helper(root);
+        return max;
+    }
+    public static int helper(TreeNode root) {
+        if(root == null)return 0;
+        int left = Math.max(0,helper(root.left));
+        int right = Math.max(0,helper(root.right));
 
-        if(count  == 0)return max ;
-        return  Math.max(0,Math.max(left,right))+root.val;
-        // if(right <= 0 && left <= 0){
-        //     max= Math.max(max,root.val) ;
-        // }
-        // else{
-        //      max = Math.max(max,right+left+root.val);
-        // }
+        max = Math.max(max,left+right+root.val);
+
+        return Math.max(left,right) +  root.val;
     }
 }
