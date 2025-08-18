9bf2d44eed00577b77d3bd20eb8200f23aa90fd7
Tejas Sinkar <tejassinkar10@gmail.com>
2025-08-18 22:24:00 +0530

    Time: 3 ms (99.31%) | Memory: 46.1 MB (54.95%) - LeetSync

--- 210-course-schedule-ii/course-schedule-ii.java
+++ 210-course-schedule-ii/course-schedule-ii.java
@@ -1,38 +1,52 @@
 class Solution {
     public int[] findOrder(int courses, int[][] prereq) {
-        int []  ans = new int [courses];
-        int curCount = 0;
+        
         ArrayList<ArrayList<Integer>> adj = new ArrayList<>();
-        for(int i = 0;i<courses;i++){
+        
+        for(int i =0;i< courses;i++){
             adj.add(new ArrayList<>());
         }
-        int [] indegree = new int[courses];
 
-        for(int [] arr:prereq){
-            adj.get(arr[1]).add(arr[0]);
-            indegree[arr[0]]++;
+        for(int [] temp : prereq){
+            adj.get(temp[1]).add(temp[0]);
         }
-        Queue<Integer> que = new LinkedList<>();
 
-        for(int i= 0;i< courses;i++){
-            if(indegree[i]==0){
-                que.add(i);
-                ans[curCount++] = i;
+        boolean [] vis = new boolean[courses];
+        boolean [] pathVis= new boolean[courses];
+
+        Stack<Integer> stk = new Stack<>();
+
+        for(int i = 0;i<courses;i++){
+            if(!vis[i]){
+                dfs(i,stk,vis,pathVis,adj);
             }
         }
-       
-       while(!que.isEmpty()){
-            int curIdx = que.poll();
 
-            for(int i : adj.get(curIdx)){
-                indegree[i]--;
-                if(indegree[i] == 0){
-                    que.add(i);
-                    ans[curCount++] = i;
+        int [] sol = new int[courses];
+        int i  = 0;
+        if(stk.size() != courses)return new int[0];
+        while(!stk.isEmpty()){
+            sol[i++] = stk.pop();
+        }
+        return sol;
+    }
+
+    private static boolean dfs(int node,Stack<Integer>stk,boolean[] vis,boolean[] pathVis,ArrayList<ArrayList<Integer>> adj){
+        vis[node] = true;
+        pathVis[node] = true;
+
+        for(int childNode : adj.get(node)){
+            if(!vis[childNode] ){
+                if(dfs(childNode,stk,vis,pathVis,adj)){
+                    return true;
                 }
             }
-       }
-       if(curCount == courses)return ans;
-       return new int[0];
+            else if(pathVis[childNode]){
+                return true;
+            }
+        }
+        stk.push(node);
+        pathVis[node] = false;
+        return false;
     }
 }
