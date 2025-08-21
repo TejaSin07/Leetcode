cb9300a9bf16ea31aa87c056ec483d4c10981393
Tejas Sinkar <tejassinkar10@gmail.com>
2025-08-21 18:13:40 +0530

    Time: 2 ms (82.69%) | Memory: 45.2 MB (79.45%) - LeetSync

---
+++ 967-minimum-falling-path-sum/minimum-falling-path-sum.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int minFallingPathSum(int[][] matrix) {
+        int len = matrix.length; //3
+        int ans = Integer.MAX_VALUE;
+        int [][]  dp = new int[len][len];
+        boolean [][] vis = new boolean[len][len];
+        int [][] dir = {{1,-1},{1,0},{1,1}};
+        for(int i =0;i< len;i++){
+            ans = Math.min(helper(0,i,dp,matrix,dir,vis),ans);
+        }
+        return ans;
+    }
+
+
+    private static int helper(int i,int j,int[][] dp,int [][] matrix,int [][]dir,boolean [][] vis){
+        if(i == dp.length -1){
+            dp[i][j] = matrix[i][j];
+            vis[i][j] = true;
+            return dp[i][j];
+        }
+        int ans = Integer.MAX_VALUE;
+        if(vis[i][j])return dp[i][j];
+
+        for(int temp[] : dir ){
+            int r =  i+temp[0];
+            int c = j+temp[1];
+            if(r >= dp.length || c < 0 || c >= dp.length )continue;
+            
+            ans = Math.min(helper(r,c,dp,matrix,dir,vis),ans);
+            
+        }
+        dp[i][j] = ans+matrix[i][j];
+        vis[i][j] = true;
+        
+        return dp[i][j];
+
+    }
+}
