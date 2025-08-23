25423c9792b383a60f1ca272bd235be8a55c994b
Tejas Sinkar <tejassinkar10@gmail.com>
2025-08-23 21:04:34 +0530

    Time: 51 ms (45.51%) | Memory: 47.2 MB (52.23%) - LeetSync

--- 416-partition-equal-subset-sum/partition-equal-subset-sum.java
+++ 416-partition-equal-subset-sum/partition-equal-subset-sum.java
@@ -8,25 +8,30 @@ class Solution {
         }
         if(sum%2==1)return false;
         sum = sum/2;
-        int [][] dp = new int [len][sum+1];
-        for(int ar[] : dp){
-            Arrays.fill(ar,-1);
-        }
+        boolean [][] dp = new boolean [len][sum+1];
+       
         return helper(0,nums,sum,dp);
     }
 
 
-    private static boolean helper(int idx,int [] nums,int sum,int [][] dp){
-        if(sum == 0)return true;
-
-        if(idx >= nums.length  || sum < 0 )return false;
-
-        if(dp[idx][sum] != -1 )return (dp[idx][sum] == 0)?true:false;
-
-        boolean take = helper(idx+1,nums,sum- nums[idx],dp);
-        boolean ntake = helper(idx+1,nums,sum,dp);
-
-        dp[idx][sum] = (take || ntake) ? 0:1;
-        return take || ntake;
+    private static boolean helper(int idx,int [] arr,int sum,boolean [][] dp){
+        
+        for(int i = 0;i< dp.length;i++){
+            for(int j = 0 ;j< dp[0].length;j++){
+                if(j == 0 || j == arr[i]){
+                    dp[i][j]  = true;
+                    continue;
+                }
+                if( i > 0 && j - arr[i] >= 0 && dp[i-1][j-arr[i]] == true ){
+                    dp[i][j]  = true;
+                    continue;
+                }
+                if(i > 0 && dp[i-1][j] == true){
+                    dp[i][j]  = true;
+                    continue;
+                }
+            }
+        }
+        return dp[ dp.length-1][sum];
     }
 }
