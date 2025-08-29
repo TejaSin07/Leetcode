855b5c4a05a3705427df0d75b9f964a2d3983d93
Tejas Sinkar <tejassinkar10@gmail.com>
2025-08-29 12:02:15 +0530

    Time: 19 ms (59.86%) | Memory: 51.5 MB (30.26%) - LeetSync

--- 115-distinct-subsequences/distinct-subsequences.java
+++ 115-distinct-subsequences/distinct-subsequences.java
@@ -1,27 +1,33 @@
 class Solution {
     public int numDistinct(String s, String t) {
-        int len1 = s.length();
-        int len2 = t.length();
+        int n = s.length();
+        int m = t.length();
+        int mod = (int) (1e9 + 7); 
 
-        int dp[][] = new int[len1][len2];
-        for(int ar[] : dp){
-            Arrays.fill(ar,-1);
+        int[][] dp = new int[n + 1][m + 1];
+
+
+        for (int i = 0; i <= n; i++) {
+            dp[i][0] = 1;
         }
 
-        return helper(0,0,s,t,dp);
-    }
 
-    private static int helper(int idx1,int idx2,String s,String t,int [][] dp){
-        if(idx2== t.length())return 1;
-        if(idx1 == s.length())return 0;
-
-        if(dp[idx1][idx2] != -1)return dp[idx1][idx2];
-
-        int take = 0;
-        if(s.charAt(idx1) ==  t.charAt(idx2)){
-            take = helper(idx1+1,idx2+1,s,t,dp);
+        for (int j = 1; j <= m; j++) {
+            dp[0][j] = 0;
         }
-        int nontake = helper(idx1+1,idx2,s,t,dp);
-        return dp[idx1][idx2] = take+nontake;
+
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    // Include or exclude current char
+                    dp[i][j] = (dp[i - 1][j - 1] + dp[i - 1][j]) ;
+                } else {
+                    // Exclude current char
+                    dp[i][j] = dp[i - 1][j];
+                }
+            }
+        }
+
+        return dp[n][m];
     }
 }
