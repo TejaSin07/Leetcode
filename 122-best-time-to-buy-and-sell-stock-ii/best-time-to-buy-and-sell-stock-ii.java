0c16c27ad8a73e0a00fee88f0f5f33eab59bdbc2
Tejas Sinkar <tejassinkar10@gmail.com>
2025-08-30 10:54:07 +0530

    Time: 0 ms (100.00%) | Memory: 46.4 MB (14.79%) - LeetSync

--- 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
+++ 122-best-time-to-buy-and-sell-stock-ii/best-time-to-buy-and-sell-stock-ii.java
@@ -1,28 +1,11 @@
 class Solution {
     public int maxProfit(int[] prices) {
-        int n = prices.length;
-
-        // next[0] → profit if we can buy
-        // next[1] → profit if we have stock and can sell
-        int[] next = new int[2];
-        int[] curr = new int[2];
-
-        // Base case: when index == n, profit = 0
-        next[0] = next[1] = 0;
-
-        for (int index = n - 1; index >= 0; index--) {
-            // Case when we can buy
-            curr[0] = Math.max(next[0], -prices[index] + next[1]);
-
-            // Case when we can sell
-            curr[1] = Math.max(next[1], prices[index] + next[0]);
-
-            // Move curr → next for next iteration
-            next[0] = curr[0];
-            next[1] = curr[1];
+        int ans = 0;
+        for(int i = 0;i<prices.length-1;i++){
+            if(prices[i+1] > prices[i]){
+                ans += prices[i+1] - prices[i];
+            }
         }
-
-        // Start at day 0 with the option to buy
-        return next[0];
+        return ans;
     }
 }
