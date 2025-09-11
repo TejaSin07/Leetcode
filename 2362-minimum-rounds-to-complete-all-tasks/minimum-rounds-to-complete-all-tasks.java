e70eeb1c8fe103189fac2401198ad421282ea272
Tejas Sinkar <tejassinkar10@gmail.com>
2025-09-11 21:23:46 +0530

    Time: 24 ms (93.98%) | Memory: 67.4 MB (5.23%) - LeetSync

---
+++ 2362-minimum-rounds-to-complete-all-tasks/minimum-rounds-to-complete-all-tasks.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int minimumRounds(int[] tasks) {
+        int len = tasks.length;
+        int dp[] = new int [len];
+        Arrays.sort(tasks);
+        Arrays.fill(dp,Integer.MAX_VALUE);
+        int x = helper(0,tasks,dp);
+        if(x >= Integer.MAX_VALUE-500)return -1;
+        return x;
+    }
+
+
+    private static int helper(int idx,int [] tasks,int[] dp){
+
+        if(idx >= tasks.length)return 0;
+
+        if(dp[idx] != Integer.MAX_VALUE)return dp[idx];
+        int first = Integer.MAX_VALUE-500;  
+        int second = Integer.MAX_VALUE-500;
+
+        boolean f = true;
+        boolean s = true;
+        if(idx+1 < tasks.length && tasks[idx] == tasks[idx+1]){
+            first = helper(idx+2,tasks,dp);
+        }else{
+            f = false;
+        }
+        if(idx+2 < tasks.length && tasks[idx] == tasks[idx+2]){
+            second = helper(idx+3,tasks,dp);
+        }
+        else{
+            s = false;
+        }
+
+        if(!f && !s )return Integer.MAX_VALUE-500;
+         return dp[idx] = 1+ Math.min(first,second);  
+    }
+}
