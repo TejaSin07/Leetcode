8788571ba91446fd43976c2ec15b23b37c6b60e8
Tejas Sinkar <tejassinkar10@gmail.com>
2025-09-16 17:36:05 +0530

    Time: 38 ms (9.99%) | Memory: 46.3 MB (5.32%) - LeetSync

--- 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
+++ 1437-minimum-insertion-steps-to-make-a-string-palindrome/minimum-insertion-steps-to-make-a-string-palindrome.java
@@ -1,22 +1,25 @@
 class Solution {
     public int minInsertions(String s) {
         int len = s.length();
-        int dp[] = new int [len];
+        int idx1 = 0;
+        int idx2 = s.length()-1;
+        Integer[][] dp = new Integer[len][len];
 
-        for(int i = len-2;i>=0;i--){
-            int prev = 0;
-            for(int j = i+1;j<len;j++){
-                int temp = dp[j];
-                if(s.charAt(i) == s.charAt(j)){
-                    dp[j] = prev;
-                }
-                else{
-                    dp[j] = Math.min(dp[j],dp[j-1])+1;
-                }
-                prev = temp;
-                
-            }
+        return s.length() - helper(idx1,idx2,s,dp);
+    }
+
+
+    private static int helper(int idx1,int idx2,String s,Integer [][]dp){
+        if(idx1 >= s.length() ||  idx2 < 0){
+            return 0;
         }
-        return dp[len-1];
+
+        if(dp[idx1][idx2] != null )return dp[idx1][idx2];
+
+        if(s.charAt(idx1) == s.charAt(idx2)){
+            return dp[idx1][idx2] = 1+helper(idx1+1,idx2-1,s,dp);
+        }
+
+        return dp[idx1][idx2] = Math.max(helper(idx1+1,idx2,s,dp),helper(idx1,idx2-1,s,dp));
     }
 }
