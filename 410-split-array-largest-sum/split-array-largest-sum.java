9065b125e2e27b0610357f95e12ff2f173ff4d38
Tejas Sinkar <tejassinkar10@gmail.com>
2025-10-04 18:46:45 +0530

    Time: 0 ms (100.00%) | Memory: 40.9 MB (98.94%) - LeetSync

--- 410-split-array-largest-sum/split-array-largest-sum.java
+++ 410-split-array-largest-sum/split-array-largest-sum.java
@@ -1,7 +1,7 @@
 class Solution {
     public int splitArray(int[] nums, int k) {
         int low = 0;
-        int high = Integer.MAX_VALUE;
+        int high = Integer.MAX_VALUE; 
 
         while(low<=high){
             int mid = low + (high-low)/2;
@@ -19,18 +19,18 @@ class Solution {
 
     private static boolean helper(int posAns,int [] nums,int k){
 
-        int count = 1;
+        int count = 1;  //default count 
         int sum =  0;
         for(int val : nums){
             sum += val;
             if(sum > posAns){
                 count++;
                 sum = val;
-                if(sum > posAns)return false;
+                if(sum > posAns)return false;   //if still sum greater than possible ans return false
             }
         }
 
-        return count <= k;
+        return count <= k; // return true if we are in possible ans range
 
     }
 }
