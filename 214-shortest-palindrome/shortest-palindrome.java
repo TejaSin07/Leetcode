775a8bdcd22b486e8149501248f62b72a3907c38
Tejas Sinkar <tejassinkar10@gmail.com>
2025-10-14 18:21:48 +0530

    Time: 97 ms (15.83%) | Memory: 46 MB (15.44%) - LeetSync

---
+++ 214-shortest-palindrome/shortest-palindrome.java
@@ -0,0 +1,15 @@
+class Solution {
+
+    //first we find longest palindrome present 
+    //then we add 
+    public String shortestPalindrome(String s) {
+        String rev = new StringBuilder(s).reverse().toString();
+
+        for(int i = 0;i<rev.length();i++){
+            if(s.startsWith(rev.substring(i))){
+                return rev.substring(0,i) + s;
+            }
+        }
+        return rev+s;
+    }
+}
