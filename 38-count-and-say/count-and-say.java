587470cfc29c87d4d2ce0607d99a528594604ecd
Tejas Sinkar <tejassinkar10@gmail.com>
2025-10-24 08:06:41 +0530

    Time: 4 ms (55.22%) | Memory: 44.3 MB (16.79%) - LeetSync

--- 38-count-and-say/count-and-say.java
+++ 38-count-and-say/count-and-say.java
@@ -1,31 +1,57 @@
 class Solution {
     public String countAndSay(int n) {
-        if (n == 1) return "1";
-        String s = "1";
-        return rle(s,n,2);
+        String ans = "1";
+    
+        int k = 1;
+        while(k < n){
+            
+            ArrayList<ArrayList<Integer>> lst = getList(ans);
+            ans = getString(lst);
+            k++;
+        }
+        return ans;
     }
 
-    public static String rle(String s,int n,int cur){
-
-        StringBuilder str = new StringBuilder();
-
-        int count =1;
-        for(int i = 1;i<s.length();i++){
-            if(s.charAt(i)==s.charAt(i-1)){
-                count++;
-            }
-            else{
-                str.append(count);
-                str.append(s.charAt(i-1));
-                count = 1;
-            }
-
+    private static String getString(ArrayList<ArrayList<Integer>> lst){
+        StringBuilder sb = new StringBuilder();
+        for (ArrayList <Integer> row : lst) { // Outer loop iterates through each inner List (row)
+            for (Integer element : row) { // Inner loop iterates through each element in the current row
+                char ch = (char)(element + '0');
+                sb.append(ch);
+            }// Move to the next line after processing each row
         }
-        str.append(count);
-        str.append(s.charAt(s.length() - 1));
-        cur++;
-        if(cur <= n)return rle(str.toString(),n,cur);
-        return str.toString();
+        return sb.toString();
+    }
+
+
+
+    private static ArrayList<ArrayList<Integer>> getList(String str){
         
+        ArrayList<ArrayList<Integer>> lst = new ArrayList<>();
+
+        int[] intArray = new int[str.length()];
+
+        for (int i = 0; i < str.length(); i++) {
+            intArray[i] = str.charAt(i) - '0';
+        }
+        int count = 1;
+        for(int i = 1;i< intArray.length;i++){
+            
+            if(intArray[i]==intArray[i-1]){
+                count++;
+                continue;
+            }
+
+            ArrayList<Integer>temp = new ArrayList<>();
+            temp.add(count);
+            temp.add(intArray[i-1]);
+            lst.add(temp);
+            count = 1;
+        }
+            ArrayList<Integer>temp = new ArrayList<>();
+            temp.add(count);
+            temp.add(intArray[str.length()-1]);
+            lst.add(temp);
+            return lst;
     }
 }
