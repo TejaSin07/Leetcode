b41358ca45b9271ed5c33264f2e3d9aa6e8972f5
Tejas Sinkar <tejassinkar10@gmail.com>
2025-11-05 20:55:39 +0530

    Time: 0 ms (100.00%) | Memory: 46.3 MB (5.38%) - LeetSync

--- 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
+++ 25-reverse-nodes-in-k-group/reverse-nodes-in-k-group.java
@@ -14,35 +14,33 @@ class Solution {
         ListNode ans = dummy;
         dummy.next = head;
         ListNode current = head;
+        ListNode start = dummy;
+        
 
         while(current != null){
             int count = 1;
-            while(current.next != null && count < k){
+            start = current;
+            while(current.next != null && count<k){
                 current = current.next;
                 count++;
             }
-            ListNode nextDummy = dummy.next;
+            ListNode nextCurrent = current.next;
+            current.next= null;
+            if(count == k){       
+                ListNode temp = reverse(start);
+                start.next = nextCurrent;
+                dummy.next = temp;
+                dummy = start;
+            }
+            current = nextCurrent;
 
-            if(count == k){
-                ListNode nextCurrent = current.next;
-                current.next = null;
-                dummy.next = reverse(dummy.next);
-                nextDummy.next = nextCurrent;
-                current = nextCurrent;
-                dummy = nextDummy;
-            }
-            else{
-               break;
-            }
-           
         }
         return ans.next;
     }
 
-    private ListNode reverse(ListNode head ){
-        
-        ListNode prev = null;
+    private ListNode reverse(ListNode head){
 
+        ListNode prev = null;
         while(head != null){
             ListNode temp = head.next;
             head.next = prev;
