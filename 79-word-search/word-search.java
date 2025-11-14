40a32a1014f73ec77eb4fa56b378a5b4ee9092f4
Tejas Sinkar <tejassinkar10@gmail.com>
2025-11-14 19:12:20 +0530

    Time: 101 ms (95.36%) | Memory: 42.8 MB (31.99%) - LeetSync

--- 79-word-search/word-search.java
+++ 79-word-search/word-search.java
@@ -1,41 +1,38 @@
 class Solution {
     public boolean exist(char[][] board, String word) {
-        int m = board.length;//ROW
-        int n = board[0].length;//COL
-         int ind = 0;
-
-         for(int i = 0;i<m;i++){
-               for(int j = 0 ; j<n; j++){
-                   if(board[i][j] == word.charAt(ind)){
-                     if(helper(i , j ,m , n ,  ind , board , word )){
-                    return true;         
-                    }
-                 }
-            }
-       }
-       return false;  
-    }
-     public boolean helper(int row , int col ,int m , int n , int ind , char [] [] board , String word){
-        if(ind == word.length()){
-            return true;
-        }
-        if(row == m || col == n ||  row<0 || col<0  || board[row][col] == '!' ||  board[row][col] != word.charAt(ind)){
-           return false;
-        }
-        char ch =board[row][col];
-        board[row][col] = '!';
-
-        boolean top = helper(row-1 , col ,m , n ,  ind+1 , board , word );
-
-        boolean bottom = helper(row+1 , col ,m , n ,  ind+1 , board , word );
-        
-        boolean left = helper(row , col-1 ,m , n ,  ind+1 , board , word );
-
-        boolean right = helper(row , col+1 ,m , n ,  ind+1 , board , word );
-
-        board[row][col] = ch;
-         
-        return top || bottom || left || right;
-     }
     
+        boolean [][] vis = new boolean[board.length][board[0].length];
+
+        for(int i = 0;i<board.length;i++){
+            for(int j = 0;j<board[0].length;j++){
+                if(board[i][j] == word.charAt(0)){
+                    if(helper(1,i,j,vis,board,word))return true;
+                }
+            }
+        }
+        return false;
+    }
+
+
+    private boolean helper(int ind,int row,int col,boolean [][] vis,char[][] board,String word){
+        
+        if(ind == word.length())return true;
+        
+        vis[row][col] = true;
+
+        boolean right = false;
+        boolean left = false;
+        boolean up = false;
+        boolean down = false;
+
+        if( row+1 < board.length && word.charAt(ind) == board[row+1][col] && vis[row+1][col] == false )  down = helper(ind+1,row+1,col,vis,board,word);
+        if( row-1 >= 0  && word.charAt(ind) == board[row-1][col]          && vis[row-1][col] == false )    up = helper(ind+1,row-1,col,vis,board,word);
+        if( col+1 < board[0].length && word.charAt(ind) == board[row][col+1] && vis[row][col+1] == false)right = helper(ind+1,row,col+1,vis,board,word);
+        if( col-1 >= 0 && word.charAt(ind) == board[row][col-1]   && vis[row][col-1] == false )left= helper(ind+1,row,col-1,vis,board,word);
+
+        vis[row][col] = false;
+
+        return right || left || up || down;
+ 
+    }
 }
