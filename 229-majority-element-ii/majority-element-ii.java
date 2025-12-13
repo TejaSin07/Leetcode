17ec887f2db93e1661b96baead90878860d64293
Tejas Sinkar <tejassinkar10@gmail.com>
2025-12-13 23:55:15 +0530

    Time: 2 ms (99.80%) | Memory: 52.7 MB (15.51%) - LeetSync

--- 229-majority-element-ii/majority-element-ii.java
+++ 229-majority-element-ii/majority-element-ii.java
@@ -1,22 +1,43 @@
 class Solution {
     public List<Integer> majorityElement(int[] nums) {
-        List<Integer> aba = new ArrayList<>();
-        HashMap<Integer, Integer> ans = new HashMap<>();
-        int a = nums.length / 3;
-        for (int i = 0; i < nums.length; i++) {
-            if (ans.containsKey(nums[i])) {
-                ans.put(nums[i], ans.get(nums[i]) + 1);
+        List<Integer> lst = new ArrayList<>();
+        int cand1 = 0,cand2 = 0;
+        int count1 = 0,count2  = 0;
 
-            } else {
-                ans.put(nums[i], 1);
-
+        for(int ele : nums){
+            
+            if(ele == cand1){
+                count1++;
+            }
+            else if(ele == cand2){
+                count2++;
+            }
+            else if (count1 == 0 ){
+                cand1 = ele;
+                count1 = 1;
+            }
+            else if(count2 == 0){
+                cand2 = ele;
+                count2 =1;
+            }
+           
+            else{
+                count1--;
+                count2--;
             }
         }
-        for (Integer key : ans.keySet()) {
-            if (ans.get(key) > a) {
-                aba.add(key);
-            }
+
+        count1 = 0;
+        count2 = 0;
+
+        for(int ele : nums){
+            if(ele == cand1)count1++;
+            else if(ele == cand2)count2++;
         }
-        return aba;
+
+        if(count1 > nums.length/3)lst.add(cand1);
+        if(count2 > nums.length/3)lst.add(cand2);
+
+        return lst;
     }
 }
