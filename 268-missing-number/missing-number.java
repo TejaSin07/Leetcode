9f75b1d913b197803b6b0e6bb5f4172a0c336c16
Tejas Sinkar <tejassinkar10@gmail.com>
2025-12-13 19:11:18 +0530

    Time: 0 ms (100.00%) | Memory: 47.2 MB (54.27%) - LeetSync

--- 268-missing-number/missing-number.java
+++ 268-missing-number/missing-number.java
@@ -3,10 +3,9 @@ class Solution {
         int sum = 0;
         for(int i = 0;i<=nums.length;i++){
             sum += i;
-        }
-
-        for(int i : nums){
-            sum -= i;
+            if(i < nums.length){
+                sum -= nums[i];
+            }
         }
         return sum;
     }
