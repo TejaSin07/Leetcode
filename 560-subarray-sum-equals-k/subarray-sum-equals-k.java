42275c127355fe605f2870b3d21539566e14b223
Tejas Sinkar <tejassinkar10@gmail.com>
2025-12-13 22:41:17 +0530

    Time: 25 ms (46.85%) | Memory: 48.9 MB (20.77%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.java
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.java
@@ -1,38 +1,23 @@
 class Solution {
     public int subarraySum(int[] nums, int k) {
-        int count = 0 ;
-        int sum[] = new int[nums.length+1];
-        sum[0] = 0;
-        for(int i = 1;i<=nums.length;i++){
-            sum[i] = sum[i-1]+nums[i-1];
-        }
-        for(int start = 0;start<sum.length;start++){
-            for(int end = start + 1;end<sum.length;end++){
-                if(sum[end]-sum[start]==k){
-                    count++;
-                }
+        
+        int count = 0;
+        int sum = 0;
+        int len = nums.length;
+
+        Map<Integer,Integer> map = new HashMap();
+        map.put(sum,1);
+
+
+        for(int i: nums){
+            
+            sum += i;
+            if(map.containsKey(sum-k)){
+                count += map.get(sum-k);
             }
+
+            map.put(sum,map.getOrDefault(sum,0)+1);
         }
         return count;
     }
-    }
-//         Map<Integer,Integer>preSum = new HashMap<>();
-//         int result = 0;
-//         int sum = 0;
-//         preSum.put(0,1);
-//         for(int i = 0;i<nums.length;i++){
-//             sum += nums[i];
-//             if(preSum.containsKey(sum-k)){
-//                 result += preSum.get(sum-k);
-//             }
-
-//             if(preSum.containsKey(sum)){
-//                 preSum.put(sum,preSum.get(sum)+1);
-//             }
-//             else{
-//                 preSum.put(sum,1);
-//             }
-//         }
-//         return result;
-//     }
-// }
+}
