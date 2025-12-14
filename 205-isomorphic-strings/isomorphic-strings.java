609c49357c47977db17124d30ce889443998c9c1
Tejas Sinkar <tejassinkar10@gmail.com>
2025-12-14 16:20:03 +0530

    Time: 37 ms (5.59%) | Memory: 43.7 MB (48.81%) - LeetSync

--- 205-isomorphic-strings/isomorphic-strings.java
+++ 205-isomorphic-strings/isomorphic-strings.java
@@ -1,35 +1,22 @@
 class Solution {
     public boolean isIsomorphic(String s, String t) {
-       
-     int [] ar = new int [128];
-     int [] kr = new int [128]; 
-    for(int i = 0;i<s.length();i++){
-        char m = s.charAt(i);
-        char n = t.charAt(i);
-        if(ar[m]==0 && kr[n]==0){
-            ar[m] = n;
-            kr[n] = m;
+        
+        var map1=  new HashMap<Character,Character>();
+        var map=  new HashMap<Character,Character>();
+
+        for(int i = 0;i<s.length();i++){
+
+            if(map1.containsKey(s.charAt(i))){
+                if(map1.get(s.charAt(i)) != t.charAt(i))return false;
+            }
+            if(map.containsKey(t.charAt(i))){
+                if(map.get(t.charAt(i)) != s.charAt(i))return false;
+            }
+
+            map1.put(s.charAt(i),t.charAt(i));
+            map.put(t.charAt(i),s.charAt(i));
         }
-        else if (ar[m] != n || kr[n] != m){
-            return false;
-        }
-    }
-    return true;
+        return true;
+
     }
 }
-
-
-    //  int [] ar = new int [128];
-    //  int [] kr = new int [128];
-    //  int sum1= 0;
-    //  int sum2 = 0;
-    //  for(int i =0;i<s.length();i++){
-    //     ar[s.charAt(i)] += 1;
-    //     kr[t.charAt(i)] += 1;
-    //     sum1 += ar[s.charAt(i)];
-    //     sum2 += kr[t.charAt(i)] ;
-    //     if(sum1 != sum2){
-    //         return false;
-    //     }
-    //  }
-    // return true;
