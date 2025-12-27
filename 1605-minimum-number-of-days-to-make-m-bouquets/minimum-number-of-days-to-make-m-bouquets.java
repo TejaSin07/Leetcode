41ff6f5269c6d696e21b960041bf025066ca3b4e
Tejas Sinkar <tejassinkar10@gmail.com>
2025-12-27 23:44:52 +0530

    Time: 19 ms (44.65%) | Memory: 82.6 MB (37.02%) - LeetSync

--- 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
+++ 1605-minimum-number-of-days-to-make-m-bouquets/minimum-number-of-days-to-make-m-bouquets.java
@@ -1,52 +1,47 @@
 class Solution {
-    public int minDays(int[] bloomDay, int m, int k) {
-        if (((long)m * k) > bloomDay.length) return -1;
-
-        int max = bloomDay[0];
-        int min = bloomDay[0];
+    public int minDays(int[] days, int m, int k) {
         
+        int left = 1;
+        int right = Integer.MIN_VALUE;
         
-//         if (((long)m * k) > n) return -1;
-// instead of if(m*k >n) return -1;
-
-// as the range for m is given 10^6 and for k it is 
-// 10^5 so when multiplied it causes integer overflow .
-
-        for(int i : bloomDay){
-            max = Math.max(max,i);
-            min = Math.min(min,i);
+        for(int i : days){
+            right = Math.max(right,i);
         }
-        int left = min;
-        int right = max;
+        
+        int maxLtd = right;
+        int len = days.length;
+        if(len < m*k)return -1;
 
         while(left <= right){
-            int mid = left + (right - left) / 2;
-            boolean possible = calReq(bloomDay,mid,m,k);
-            if(possible){
+
+            int mid = left + (right - left)/2;
+
+            if(isPos(mid,days,m,k)){
                 right = mid-1;
             }
             else{
                 left = mid+1;
             }
         }
-        return left;
+        return left>maxLtd?-1:left;
     }
 
-    private static boolean calReq(int[]bloomDay,int mid,int m,int k){
-        int boq = 0;
-        int adj =0;
-        for(int a : bloomDay){
-            if(a <= mid){
-                adj++;
+    private boolean isPos(int mid,int [] days,int m,int n){
+        
+        int flowers = 0;
+        for(int i : days){
+
+            if(i <= mid){
+                flowers++;
+                if(flowers == n ){
+                    m--;
+                    flowers =0;
+                }
             }
             else{
-                boq += adj/k;
-                adj =0;
+                flowers = 0;
             }
-        }
-        boq += adj/k;
-        if( boq >= m){
-           return true; 
+            if(m <= 0)return true;
         }
         return false;
     }
