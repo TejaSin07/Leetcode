2615f9faad4c3d40671630576a3c8ab244707190
Tejas Sinkar <tejassinkar10@gmail.com>
2025-12-29 23:26:40 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (55.53%) - LeetSync

--- 213-house-robber-ii/house-robber-ii.java
+++ 213-house-robber-ii/house-robber-ii.java
@@ -7,25 +7,28 @@ class Solution {
 
         if(len <2)return nums[0];
         
-        int first = helper(0,nums,dp,len-1);
-        int second = helper(1,nums,dp2,len);
+        int first = helper(0,nums,len-1);
+        int second = helper(1,nums,len);
 
 
         return Math.max(first,second); 
     }
 
 
-    private int helper(int ind ,int [] nums,Integer dp [] ,int len){
+    private int helper(int ind ,int [] nums,int len){
 
-        if(ind > len-1)return 0;
+        int prev2 = 0;
+        int prev = nums[ind];
+        
 
-        if(dp[ind] != null)return dp[ind];
+        for(int i = ind+1;i<len;i++){
 
-        int take = nums[ind] + helper(ind+2,nums,dp,len);
+            int cur = Math.max(nums[i] + prev2,prev);
+            prev2 = prev;
+            prev = cur;
 
-        int notTake = helper(ind+1,nums,dp,len);
-
-        return dp[ind]= Math.max(take,notTake);
+        }
+        return Math.max(prev2,prev);
 
     }
 }
